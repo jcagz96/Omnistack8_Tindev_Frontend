4dc83e58e9b4d89b3693f3682ab7c51c7f0cd529
jcagz96 <jcagz@iscte-iul.pt>
2019-08-10 18:23:35 +0100

    heroku initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
+/*
 const api = axios.create({
     baseURL: 'http://localhost:3333'
+})
+*/
+
+const api = axios.create({
+    baseURL: 'https://hidden-mountain-22247.herokuapp.com'
 })
 
 export default api
